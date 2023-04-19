d530680df2eab8b88394cbb6fc8e1857f286e663
kassem santos <kassem.santos@fatec.sp.gov.br>
2023-04-19 12:38:21 -0300

    atualizado_19_04_23

--- Disciplina.java
+++ Disciplina.java
@@ -1,13 +1,35 @@
 public class Disciplina {
     private ICalcMedia Nota;
 
+    private double m;
+    private String nome;
     private double a;
     private double b;
-    private double m;
+    private String situacao;
 
     public ICalcMedia getNota(){
         return Nota;
     }
 
-    
+    public void setNota(ICalcMedia Nota){
+        Nota = Nota;
+    }
+
+    public double getA(){
+        return a;
+    }
+
+    public void setA(double a){
+        this.a = a;
+    }
+
+    public double getB(){
+        return b;
+    }
+
+    public void setB(double b){
+        this.b = b;
+    }
+
+
 }
