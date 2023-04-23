9859b581915e5ab903910f6228fb16157b7ba664
Kassemsantos <kassem.santos@fatec.sp.gov.br>
2023-04-23 11:57:21 -0300

    exer

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
-public class public static void main(String[] args) {
-    Aritmetica calculo = new Aritmetica();
-    Geometrica calculoo = new Geometrica();
+public class Main{
+    public class public static void main(String[] args) {
+        Aritmetica calculo = new Aritmetica();
+        Geometrica calculoo = new Geometrica();
 
-    Disciplina d = new Disciplina(calculo);
+        Disciplina d = new Disciplina(calculo);
 
-    d.setNome("Padroes de desenvolvimento");
-    d.setA(10); d.setB(5);
-    d.calculoMedia();
-    System.out.println(
-        String.format("P1:%.2f  P2:%.2f  Media:%.2f  Situação: %s",
-        d.getA(), d.getB(), d.getM(), d.getSituacao()));
+        d.setNome("Padroes de desenvolvimento");
+        d.setA(10); d.setB(5);
+        d.calculoMedia();
+        System.out.println(
+            String.format("P1:%.2f  P2:%.2f  Media:%.2f  Situação: %s",
+            d.getA(), d.getB(), d.getM(), d.getSituacao()));
 
 
+    }
 }
