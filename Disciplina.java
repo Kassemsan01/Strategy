50847b0cee98d4fa67c4b5e7dfddad41b2362834
Kassemsantos <kassem.santos@fatec.sp.gov.br>
2023-04-23 18:06:45 -0300

    exer

--- Disciplina.java
+++ Disciplina.java
@@ -15,6 +15,50 @@ public class Disciplina {
         return Nota;
     }
 
+    public void setM(double m){
+        this.m = m;
+    }
+
+    public double getM(){
+        return m;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setA(double a){
+        this.a = a;
+    }
+
+    public double getA(){
+        return a;
+    }
+
+    public void setB(double b){
+        this.b = b;
+    }
+
+    public double getB(){
+        return b;
+    }
+
+    public void setSituacao(String situacao){
+        this.situacao = situacao;
+    }
+
+    public String getSituacao(){
+        return situacao;
+    }
+
+    public double calcular()
+    {
+        return this.Nota.calculoMedia(this.a, this.b);
+    }
 
 
 }
