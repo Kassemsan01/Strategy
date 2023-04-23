ece552f6de62f70aa5022673e0f2792752885395
Kassemsantos <kassem.santos@fatec.sp.gov.br>
2023-04-23 14:38:17 -0300

    exer

--- Disciplina.java
+++ Disciplina.java
@@ -7,57 +7,13 @@ public class Disciplina {
     private double b;
     public String situacao;
 
+    public void setNota(ICalcMedia Nota){
+        this.Nota = Nota;
+    }
+
     public ICalcMedia getNota(){
         return Nota;
     }
-
-    public void setNota(ICalcMedia Nota){
-        Nota = Nota;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(double m){
-        this.nome = nome;
-
-    public double getM(){
-        return m;
-    }
-
-    public void setM(double m){
-        this.m = m;
-    }
-
-    public double getA(){
-        return a;
-    }
-
-    public void setA(double a){
-        this.a = a;
-    }
-
-    public double getB(){
-        return b;
-    }
-
-    public void setB(double b){
-        this.b = b;
-    }
-
-    public String getSituacao(){
-        return situacao;
-    }
-
-    public void setSituacao(String situacao){
-        this.situacao = situacao;
-    }
-
-    public double calcular(){
-        return this.Nota.calculo(this.a, this.b);
-    }
-
 
 
 
